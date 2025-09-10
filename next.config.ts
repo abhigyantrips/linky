b2532d73e2f7f971aaf079ceb0905981273ef133
Abhigyan Trips <contact@abhigyantrips.dev>
2025-09-10 12:08:01 +0530

    feat(ui): init styling and login page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://abhi.now',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/(ui)/login/page.tsx
+++ src/app/(ui)/login/page.tsx
@@ -1,22 +1,49 @@
 'use client';
 
+import { QRCodeSVG } from 'qrcode.react';
+
 import { authClient } from '@/lib/client';
 
 import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button
-        onClick={async () =>
-          await authClient.signIn.social({
-            provider: 'google',
-            callbackURL: '/dashboard',
-          })
-        }
-      >
-        Log In
-      </Button>
+      <Card className="bg-neutral-50">
+        <CardContent className="flex flex-col gap-4 md:flex-row md:gap-8">
+          <Card className="aspect-square size-full bg-white">
+            <CardContent>
+              <QRCodeSVG
+                className="size-full"
+                value="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              />
+            </CardContent>
+          </Card>
+
+          <div className="flex max-w-xs flex-col items-start justify-between md:max-w-sm">
+            <div className="flex flex-col space-y-1 md:space-y-2">
+              <h2 className="text-2xl font-bold md:text-3xl">linky.</h2>
+              <p className="text-muted-foreground text-sm md:text-base">
+                turn links into smaller links so that people can click on
+                smaller links and then get redirected to bigger links.
+              </p>
+            </div>
+            <div className="flex w-full justify-end">
+              <Button
+                onClick={async () =>
+                  await authClient.signIn.social({
+                    provider: 'google',
+                    callbackURL: '/dashboard',
+                  })
+                }
+              >
+                Log In
+              </Button>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
